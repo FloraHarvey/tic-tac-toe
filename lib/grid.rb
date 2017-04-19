4209e398f6c233ca9cb76c0ed50bbc8d98b4d8a1
Flora Harvey <fharvey555@gmail.com>
2017-04-19 22:25:27 +0100

    Inject cell class into grid and create doubles in tests

--- lib/grid.rb
+++ lib/grid.rb
@@ -2,18 +2,22 @@ class Grid
 
   attr_reader :cells
 
-  def initialize
+  def initialize(cell)
     @cells = {}
-    generate_grid
+    generate_grid(cell)
+  end
+
+  def place(mark, cell)
+    @cells[cell].place(mark)
   end
 
 
   private
 
-  def generate_grid
+  def generate_grid(cell)
     [*"A".."C"].each do |l|
       [*1..3].each do |n|
-        @cells["#{l}#{n}".to_sym] = Cell.new(l, n)
+        @cells["#{l}#{n}".to_sym] = cell.new(l, n)
       end
     end
   end
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -2,14 +2,18 @@ require 'grid'
 
 describe Grid do
 
-  let(:grid) { Grid.new }
+  let(:cell){double :cell, content: :nil, :place => nil, :X => nil, :Y => nil}
+  let(:cell_class){double :cell_class, :new => cell}
+  let(:grid) { Grid.new(cell_class) }
+
 
   it "has 9 cells" do
     expect(grid.cells.length).to eq 9
   end
 
-  it "has coordinates" do
-    expect(grid.cells[:A1].X).to eq "A"
+  it "can place a mark in a given cell" do
+    expect(grid.cells[:A1]).to receive(:place)
+    grid.place("X", :A1)
   end
 
 
